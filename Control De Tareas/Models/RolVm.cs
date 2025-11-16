0e07afa95c96a1779f7e8b7e2f5abc9b88c9d993
NormanR01 <norman.medina@ujcv.edu.hn>
2025-11-16 01:06:45 -0600

    Crear modelos base: User, Role, UserRole

---
+++ Control De Tareas/Models/RolVm.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Control_De_Tareas.Models
+{
+    public class RolVm
+    {
+        public Guid RoleId { get; set; }
+        public string RoleName { get; set; }
+
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        public DateTime CreateAt { get; set; }
+        public bool IsSoftDeleted { get; set; }
+    }
+}
---
+++ Control De Tareas/Models/UsersVm.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Control_De_Tareas.Models
+{
+    public class UserVm
+    {
+        public Guid UserId { get; set; }
+        public string Username { get; set; }
+        public string FullName { get; set; }
+
+        [EmailAddress(ErrorMessage = "Formato de email inválido")]
+        public string Email { get; set; }
+
+        [StringLength(255, MinimumLength = 6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool IsActiveBol { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
